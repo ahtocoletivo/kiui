4251c1b387446c9f3ce202f0265d8303a70d9740
Nelito <ngvjqp@gmail.com>
2018-06-28 00:43:07 +0000

    Added Update status on Get requests to API

--- apir.php
+++ apir.php
@@ -1,17 +1,38 @@
 <?php
-$con = @mysqli_connect('localhost', 'pizzapizza', 'facapartedesseahto', 'kiuipizza');
-if (!$con) {
-    echo "Error: " . mysqli_connect_error();
-  exit();
+//var_dump(run_query('select * from pedido inner join pedido_has_pizza on pedido_has_pizza.pedido_ID = pedido.ID inner join pizza on pizza.id = pedido_has_pizza.pizza_id inner join pizza_has_sabor on pizza_has_sabor.pizza_id = pizza.id inner join sabor on sabor.idsabor = pizza_has_sabor.sabor_idsabor;', $con));
+if(isset($_GET['update'])){
+  update($_GET['id'], $_GET['status']);
 }
-var_dump(run_query('select * from pedido inner join pedido_has_pizza on pedido_has_pizza.pedido_ID = pedido.ID inner join pizza on pizza.id = pedido_has_pizza.pizza_id inner join pizza_has_sabor on pizza_has_sabor.pizza_id = pizza.id inner join sabor on sabor.idsabor = pizza_has_sabor.sabor_idsabor;', $con));
 
-function run_query($query, $con){
+function run_query($query){
+  $con = @mysqli_connect('localhost', 'pizzapizza', 'facapartedesseahto', 'kiuipizza');
+  if (!$con) {
+      echo "Error: " . mysqli_connect_error();
+    exit();
+  }
   $request  = mysqli_query($con, $query);
   $data = array();
-  while ($row = mysqli_fetch_array($request)){
-    $data[] = $row;
+  if(mysqli_num_rows($request)) {
+    while ($row = mysqli_fetch_array($request)){
+      $data[] = $row;
+    }
+  }else{
+    echo "No data to return";
   }
   return $data;
+}
+
+function update($id, $status){
+  $query =  "update pedido set status =" . $status ." where ID=" . $id;
+  run_query($query);
+}
+
+function connect(){
+  $con = mysqli_connect('localhost', 'pizzapizza', 'facapartedesseahto', 'kiuipizza');
+  if (!$con) {
+      echo "Error: " . mysqli_connect_error();
+    exit();
+  }
+  return $con;
 }
 ?>
