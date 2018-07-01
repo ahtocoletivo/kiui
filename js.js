cfec18f7a1e02e6586413c1008a6ea665a382f32
Lucas Mordzin <mordzin@gmail.com>
2018-07-01 11:01:00 -0300

    Trabalhos de sabado

--- js.js
+++ js.js
@@ -24,9 +24,24 @@
     }
 }, false);
 
+sabor1 = $("#sabor1").val();
+sabor2 = $("#sabor2").val();
+sabor3 = $("#sabor3").val();
+
+size = 'Grande';
+quantidadeSabores = '2 Sabores';
+dough = 'Massa Tradicional';
+crust = 'Chocolate';
+
+$("#numSabores").text(quantidadeSabores);
+$("#tamanho").text(size);
+$("#massa").text(dough);
+$("#borda").text(crust);
+
 var userName = $("#userName").val();
 var userEmail = $("#userEmail").val();
 var userPhone = $("#userPhone").val();
+
 var userZipcode = $("#userZipcode").val();
 var userCity = $("#userCity").val();
 var userDistrict = $("#userDistrict").val();
@@ -34,5 +49,28 @@ var userStreet = $("#userStreet").val();
 var userStreetNumber = $("#userStreetNumber").val();
 var userComplement = $("#userComplement").val();
 
-var pedido = {userName: userName, userEmail: userEmail};
+var orderSubtotal = $("#orderSubtotal").val();
+var orderTotal = $("#orderTotal").val();
+var orderChange = $("#orderChange").val();
+var orderDeliveryTax = 100;
+var orderPaymentMethod = 1;
+
+var userInfo = {userName: userName, userEmail: userEmail};
 var userAdress = {userZipcode: userZipcode, userCity: userCity, userDistrict: userDistrict, userStreet: userStreet, userStreetNumber: userStreetNumber, userComplement : userComplement};
+// var orderAdress = {oderZipcode: oderZipcode, oderCity: oderCity, oderDistrict: oderDistrict, oderStreet: oderStreet, oderStreetNumber: oderStreetNumber, oderComplement : oderComplement};
+// var order = {userInfo, orderAdress};
+
+switch(quantidadeSabores) {
+    case 1:
+        $("#pizza").html(umSabor);
+        break;
+    case 2:
+        $("#pizza").html(doisSabores);
+        break;
+    case 3:
+        $("#pizza").html(tresSabores);
+        break;
+    case 4:
+        $("#pizza").html(quatroSabores);
+        break;
+}
