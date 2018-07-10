b1acae8ef8cfe4638874f73003309ec492709b0f
Nelito <ngvjqp@gmail.com>
2018-07-10 00:49:33 +0000

    Ajuste logica pizza.js

--- pizzas.js
+++ pizzas.js
@@ -1,13 +1,13 @@
 
-// SVG das pizzas
+$( document ).ready(function() {
 
 // var umSabor='<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 360 360"> <defs> <style>#imgSaborA{clip-path:url(.fillSaborA);}</style> <clipPath class="fillSaborA"> <circle cx="180" cy="180" r="180" transform="translate(0)"/> </clipPath> </defs> <g id="imgSaborA"> <image id="saborA" width="360" height="360" xlink:href="img/pizza.png" data-target="cardapio" data-toggle="modal"/> </g> </svg>';
 var umSabor='<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 360 360"> <defs> <style>#saborA{clip-path:url(.fillSaborA);}</style> <clipPath class="fillSaborA"> <circle cx="180" cy="180" r="180" transform="translate(0)"/> </clipPath> </defs> <g id="saborA"> <image width="360" height="360" xlink:href="img/pizza.png" data-target="cardapio" data-toggle="modal"/> </g></svg>';
-size = 'Grande';
-nSabores = '1';
-dough = 'Massa Tradicional';
-crust = 'Chocolate';
-
+var size = 'Grande';
+var nSabores = '1';
+var dough = 'Massa Tradicional';
+var crust = 'Chocolate';
+// SVG das pizzas
 $("#quantidadeSabores").text(nSabores);
 $("#tamanho").text(size);
 $("#massa").text(dough);
@@ -16,13 +16,6 @@ $("#borda").text(crust);
 var saborPizza = ["saborA", "saborB", "saborC", "saborD"];
 
 var current = 0;
-
-// Identifica qual fatia foi selecionada
-
-$("#pizza>svg>[id^='sabor']").click(function(){
-	current = this.id
-	alert('Você selecionou o ' + current);
-});
 
 // Adiciona sabor do #cardápio na var saboresPizza
 
@@ -47,22 +40,39 @@ $("#umSabor").click(function(){
 	nSabores = '1';
 	$('#quantidadeSabores').text(nSabores);
 	$('#pizza').html(umSabor);
+	$("#pizza>svg>[id^='sabor']").click(function(){
+		current = this.id;console.log('oi');
+		alert('Você selecionou o ' + current);
+	});
 });
 
 $("#doisSabores").click(function(){
 	nSabores = '2';
 	$('#quantidadeSabores').text(nSabores);
 	$('#pizza').html(doisSabores);
+	$("#pizza>svg>[id^='sabor']").click(function(){// Identifica qual fatia foi selecionada
+		current = this.id;console.log('oi');
+		alert('Você selecionou o ' + current);
+	});
 });
 
 $("#tresSabores").click(function(){
 	nSabores = '3';
 	$('#quantidadeSabores').text(nSabores);
 	$('#pizza').html(tresSabores);
+	$("#pizza>svg>[id^='sabor']").click(function(){// Identifica qual fatia foi selecionada
+		current = this.id;console.log('oi');
+		alert('Você selecionou o ' + current);
+	});
 });
 
 $("#quatroSabores").click(function(){
 	nSabores = '4';
 	$('#quantidadeSabores').text(nSabores);
 	$('#pizza').html(quatroSabores);
+	$("#pizza>svg>[id^='sabor']").click(function(){// Identifica qual fatia foi selecionada
+		current = this.id;console.log('oi');
+		alert('Você selecionou o ' + current);
+	});
+});
 });
